f11528961ce8feea99efd6931bbd96306000b777
Blendi Grajqevci <156774852+BlendiGR@users.noreply.github.com>
2025-05-05 22:33:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,25 +11,24 @@ import time
 class MainMenu:
     def __init__(self):
         self.running = False
-        self.monitor = HeartbeatMonitor(26, 200)
+        self.monitor = HeartbeatMonitor(26, 200) #pin and sampling rate
         self.hrv_monitor = HRV_Monitor(self.monitor)
         self.enc = Encoder()
         self.history = History(self.enc)
         self.network = Network("KMD652_Group_3", "BlendiFaiezeBlendi", "192.168.3.253")
+        self.ui = UI(self.options, self.selected, self.monitor.get_bpm())
         
         self.last_display_time = 0
-        self.last_ppg_time = 0
-        self.options = ["HEARTRATE", "HRV ANALYSIS", "ADVANCED HRV", "HISTORY"]
-
+        self.last_ppg_time = 0 
+        self.intervals = self.hrv_monitor.intervals
+        self.hrv_metrics = None
+        
+        self.options = ["HEARTRATE", "HRV ANALYSIS", "ADVANCED HRV", "HISTORY"] 
         self.selected = 0
         self.current_menu = "main"
-        self.intervals = self.hrv_monitor.intervals
         
-        self.ui = UI(self.options, self.selected, self.monitor.get_bpm())
-        self.hrv_metrics = None
-        self.id = 0
+        self.id = 0 #ID for when sending kubios requests
         
-        self.history_selected = 0
 
     def draw_options(self):
         self.ui.selected = self.selected
@@ -70,7 +69,7 @@ class MainMenu:
                             self.draw_options()
                         elif self.selected == 3:
                             self.history.run()
-                            
+                    """ Start Calculating HRV, when user presses Start """        
                     elif self.current_menu == "hrv":
                         self.network.connect_mqtt()
                         self.current_menu = "measuring_hrv"
@@ -81,6 +80,7 @@ class MainMenu:
                         )
                         self.hrv_metrics = results[1]
                         self.network.send_message("hrv/metrics", self.hrv_metrics)
+                        """ Show results to user until user presses Button to leave """
                         while True:
                             self.ui.display_hrv_metrics(self.hrv_metrics)
                             if self.enc.fifo.has_data():
@@ -90,12 +90,13 @@ class MainMenu:
                             await asyncio.sleep(0.1)
                         self.current_menu = "main"
                         self.draw_options()
+                    """ If user is currently measuring heartrate, stop it """    
                     elif self.current_menu == "heart_rate":
                         self.running = False
                         self.monitor.stop()
                         self.current_menu = "main"
                         self.draw_options()
-                        
+                    """ Start gathering data and send it to kubios cloud when user presses Start """
                     elif self.current_menu == "kubios":
                         self.network.connect_mqtt(21883)
                         results = await asyncio.gather(
@@ -106,9 +107,9 @@ class MainMenu:
                         self.intervals = self.hrv_monitor.intervals
                         kubios_response = self.network.send_kubios(self.id, self.intervals)
                         kubios_extracted = self.ui.kubios_extract(kubios_response)
-                        self.history.append_metrics_to_history(kubios_extracted, self.ui.latest_time)
-        
-                        
+                        self.history.append_metrics_to_history(kubios_extracted, self.ui.latest_time) # Add it to the history so the user can view later
+            
+                    """ Display results until user presses Button to leave """
                         while True:
                             self.ui.display_kubios(kubios_extracted)
                             if self.enc.fifo.has_data():
@@ -118,7 +119,8 @@ class MainMenu:
                             await asyncio.sleep(0.1)
                         self.current_menu = "main"
                         self.draw_options()
-                        
+                
+            """ handle the real-time BPM and graph drawing when measuring heartrate"""
             if self.current_menu == "heart_rate" and self.running:
                 self.monitor.process()
                 current_time = time.ticks_ms()
